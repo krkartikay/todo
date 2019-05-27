3aa1ad28075c4ec987e9ae8bb7b9c8376d2360b3
Mayank Gautam <mayankgautam98@gmail.com>
2019-05-27 18:16:41 +0530

    Added redux and implemented adding todos

--- v2_react/src/TodoApp.js
+++ v2_react/src/TodoApp.js
@@ -1,36 +1,65 @@
 import React from 'react';
 import './TodoApp.css';
+import { connect } from 'react-redux'; // to connect a component to redux store
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            todos: [
-                {
-                    "todoid": 5,
-                    "title": "gotta go fast",
-                    "timestamp": 1558940416.3939812
-                },
-            ]
-        };
+        this.handleClick = this.handleClick.bind(this);
+        // state will be moved to redux store
+
+        // this.state = {
+        //     todos: [
+        //         {
+        //             "todoid": 5,
+        //             "title": "gotta go fast",
+        //             "timestamp": 1558940416.3939812
+        //         },
+        //     ]
+        // };
+
     }
     render() {
         return (
             <>
                 <ul>
-                {this.state.todos.map((todoData) => <TodoItem data={todoData} />)}
+                {/* well react supports multiline js comments
+                    we will be using redux store instead of state and the data for this component will be provided as a prop 
+                    this.state.todos.map((todoData) => <TodoItem data={todoData} />)
+                */}
+
+                    {this.props.todos.map((todoData) => <TodoItem data={todoData} />)}
+
                 </ul>
                 <input 
                     id="task"
                     placeholder="Enter task"
-                    style={{display:"inline-block"}}></input>
-                <button onClick={this.handleclick}>+</button>
+                    style={{display:"inline-block"}}
+                    ref={input => this._name = input}></input>
+                <button onClick={this.handleClick}>+</button>
             </>
         );
     }
 
-    handleclick() {
+    handleClick(e) {
         // handle clicking of + button
+        //e.preventDefaults();
+        let text = this._name.value;
+        console.log(text);
+        let len = this.props.todos.length+1;
+        console.log(len,this.props.todos);
+        let newObj = {
+            type: "ADD",
+            title: text,
+            timestamp: Date.now(),
+            todoid: len
+        };
+
+        // lucky for us that as props we have also been provided a dispatch method
+        // this will pass desire object as action to the reducer
+        this.props.dispatch(newObj);
+
     }
 }
 
@@ -42,6 +71,19 @@ function TodoItem(props) {
     );
 }
 
+// this function is expected by connect (though optional)
+// this function recieves state from inside the store
+// function will return an object where keys will corrospond to props to pass to connected component
+function mapStateToProps(state) {
+    return {
+        todos : state.todos
+    };
+
+}
 
 
-export default App;
+// export default App;
+export default connect(mapStateToProps)(App);
+// here connect is a higher order component
+// in the end we export a connected App container
+
--- v2_react/src/index.js
+++ v2_react/src/index.js
@@ -5,12 +5,54 @@ import * as serviceWorker from './serviceWorker';
 import TodoApp from './TodoApp';
 import './index.css';
 
+// let's use redux now
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// it's usual to define initial state in some other file 
+const initialState = {
+    todos: [
+                {
+                    "todoid": 1,
+                    "title": "gotta go fast",
+                    "timestamp": 1558940416.3939812,
+                    "action": "ADD"
+                },
+            ]
+};
+
+// in redux terminology reducer is just a function (by chance we have named our function reducer but any name will do)
+// reducer is also generally implemented in a seprate file but we have defined it here
+// reducer expects old state and an action
+function reducer(state = initialState, action) {
+    switch(action.type) {
+    
+        case "ADD" : {
+            let arr = [...state.todos];
+            arr.push(action);
+            console.log(arr);
+            return {
+                todos: arr
+            };
+        }
+
+        default : {
+            return state;
+        }
+    }
+}
+
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <div>
-        <h1>To-Do's</h1>
-        <TodoApp/>
-        <hr/>
-    </div>,
+    <Provider store={store}>
+        <div>
+            <h1>To-Do's</h1>
+            <TodoApp/>
+            <hr/>
+        </div>
+    </Provider>,
     document.getElementById('root'));
 
 serviceWorker.register();
