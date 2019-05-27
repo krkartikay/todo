7adffba8c268db320ca71e6fd6b3f7c4dc6738ee
Kumar Karikay <krkartikay@gmail.com>
2019-05-27 12:19:36 +0530

    add heading to page

--- v2_react/src/index.js
+++ v2_react/src/index.js
@@ -5,6 +5,12 @@ import * as serviceWorker from './serviceWorker';
 import TodoApp from './TodoApp';
 import './index.css';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <h1>To-Do's</h1>
+        <TodoApp/>
+        <hr/>
+    </div>,
+    document.getElementById('root'));
 
 serviceWorker.register();
