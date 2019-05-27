c0d04e0c38a759663cc282930202e527c5cba211
Kumar Karikay <krkartikay@gmail.com>
2019-05-27 12:41:21 +0530

    add button and input

--- v2_react/src/TodoApp.js
+++ v2_react/src/TodoApp.js
@@ -4,14 +4,33 @@ import './TodoApp.css';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { todos: [{ "todoid": 5, "title": "gotta go fast", "timestamp": 1558940416.3939812 }]};
+        this.state = {
+            todos: [
+                {
+                    "todoid": 5,
+                    "title": "gotta go fast",
+                    "timestamp": 1558940416.3939812
+                },
+            ]
+        };
     }
     render() {
         return (
-            <ul>
+            <>
+                <ul>
                 {this.state.todos.map((todoData) => <TodoItem data={todoData} />)}
-            </ul>
+                </ul>
+                <input 
+                    id="task"
+                    placeholder="Enter task"
+                    style={{display:"inline-block"}}></input>
+                <button onClick={this.handleclick}>+</button>
+            </>
         );
+    }
+
+    handleclick() {
+        // handle clicking of + button
     }
 }
 
